3aabf432b33d98ed287476c7dff3f0af15875912
Luminda Saranga <sluminda@gmail.com>
2024-07-14 19:16:30 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.discipline.drms {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.discipline.drms to javafx.fxml;
+    exports com.discipline.drms;
+}
