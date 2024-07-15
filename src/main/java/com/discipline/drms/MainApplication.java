48c75cc28de8cdb4717c699fe50b5cd517277609
Luminda Saranga <sluminda@gmail.com>
2024-07-15 14:20:02 +0530

    New Update

--- src/main/java/com/discipline/drms/MainApplication.java
+++ src/main/java/com/discipline/drms/MainApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/discipline/drms/interfaces/windows/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/discipline/drms/interfaces/panelPage.fxml"));
         Parent root = loader.load();
         primaryStage.setMinWidth(850);
         primaryStage.setMinHeight(650);
